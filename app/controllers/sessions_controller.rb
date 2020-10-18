aee4a1daaf8686d0d0ee016467b40108f4b30713
sato.kento <kent@djf.co.jp>
2020-10-18 07:06:53 +0000

    user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email, password)
+      flash[:success] = 'ログインに成功しました。'
+      redirect_to "https://e55e028758684d4580c68ef5a2422b92.vfs.cloud9.us-east-1.amazonaws.com/"
+    else
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
+  end
+  
+  private
+
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      # ログイン成功
+      session[:user_id] = @user.id
+      return true
+    else
+      # ログイン失敗
+      return false
+    end
+  end
+end
